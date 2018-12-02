455123f7eef44b33f03e30bcb78e03650e6cd39d
Niko Solihin <nikosolihin@gmail.com>
2018-12-02 22:17:40 +0700

    add form submit via ajax

--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -1,3 +1,16 @@
+import qs from 'qs';
+import axios from 'axios';
+import site from '../../config/site';
 import '../stylesheets/style.scss';
 
 // Static site - Homework 4 code below
+axios.post(
+  site.url,
+  qs.stringify({
+    name: 'Axios',
+    email: 'axios@gmail.com',
+    role: 'leader',
+    message:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem pariatur provident eaque, obcaecati laudantium officia dolores maxime illum assumenda. Dolor tenetur saepe voluptatem iure suscipit hic labore, consequuntur cum at.',
+  })
+);
