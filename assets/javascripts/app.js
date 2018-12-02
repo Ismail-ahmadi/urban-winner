00706188e1fb74980bd28c1fab96623527189ab0
Niko Solihin <nikosolihin@gmail.com>
2018-12-02 22:26:49 +0700

    add success and failed message

--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -4,13 +4,16 @@ import site from '../../config/site';
 import '../stylesheets/style.scss';
 
 // Static site - Homework 4 code below
-axios.post(
-  `${site.url}/one`,
-  qs.stringify({
-    name: 'Axios',
-    email: 'axios@gmail.com',
-    role: 'leader',
-    message:
-      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem pariatur provident eaque, obcaecati laudantium officia dolores maxime illum assumenda. Dolor tenetur saepe voluptatem iure suscipit hic labore, consequuntur cum at.',
-  })
-);
+axios
+  .post(
+    `${site.url}/one`,
+    qs.stringify({
+      name: 'Axios',
+      email: 'axios@gmail.com',
+      role: 'leader',
+      message:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem pariatur provident eaque, obcaecati laudantium officia dolores maxime illum assumenda. Dolor tenetur saepe voluptatem iure suscipit hic labore, consequuntur cum at.',
+    })
+  )
+  .then(_ => console.log('success'))
+  .catch(_ => console.log('failed'));
