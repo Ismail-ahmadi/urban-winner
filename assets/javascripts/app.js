311fee1f64fd1c827f75850fd8f82f30c40a9e1b
Niko Solihin <nikosolihin@gmail.com>
2018-12-03 21:20:02 +0700

    hmm

--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -4,19 +4,19 @@ import * as config from '../../config/config.json';
 import '../stylesheets/style.scss';
 
 // Static site - Homework 4 code below
-const submit = document.querySelector('.form__submit');
+const form = document.querySelector('.form');
 const formData = {
   name: 'Milo and Finn',
   email: 'milo+finn@gmail.com',
-  role: 'follower',
-  message:
-    'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem pariatur provident eaque, obcaecati laudantium officia dolores maxime illum assumenda. Dolor tenetur saepe voluptatem iure suscipit hic labore, consequuntur cum at.',
+  message: 'Testing',
 };
 
-const submitForm = data =>
+const handleSubmit = e => {
+  e.preventDefault();
   axios
-    .post(config.site.url, qs.stringify({ 'form-name': config.form.name, ...data }))
+    .post(config.site.url, qs.stringify({ 'form-name': config.form.name, ...formData }))
     .then(_ => console.log('success'))
     .catch(_ => console.log('failed'));
+};
 
-// submit.addEventListener('click', submitForm(formData));
+form.addEventListener('submit', handleSubmit);
