555703b2c0a6d7c99444d3357224657c2efdf2e2
Niko Solihin <nikosolihin@gmail.com>
2018-12-10 15:49:41 +0700

    add id locale

--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -1,21 +1,12 @@
 import qs from 'qs';
 import axios from 'axios';
-import i18next from 'i18next';
 import '../stylesheets/style.scss';
 import * as config from '../../config/config.json';
 
-const french = document.querySelector('.locale button');
-
-const handleSubmit = e => {
-  e.preventDefault();
-  axios
-    .post(config.site.url, qs.stringify({ 'form-name': config.form.name, ...formData }, { arrayFormat: 'brackets' }))
-    .then(_ => console.log('success'))
-    .catch(_ => console.log('failed'));
-};
-
-french.addEventListener('click', e => {
-  i18next.changeLanguage('fr');
-});
-
-i18next.on('languageChanged', () => console.log('translating'));
+// const handleSubmit = e => {
+//   e.preventDefault();
+//   axios
+//     .post(config.site.url, qs.stringify({ 'form-name': config.form.name, ...formData }, { arrayFormat: 'brackets' }))
+//     .then(_ => console.log('success'))
+//     .catch(_ => console.log('failed'));
+// };
--- pages/id/tentang.md
+++ pages/id/tentang.md
@@ -1,10 +1,11 @@
 ---
-title: About Us
-slug: about
-tags: nav
+title: Tentang Kami
+slug: tentang-kami
+tags: navId
+lang: id
 summary: Lorem ipsum dolor sit amet, consectetur adipisicing elit.
 ---
-## About Us
+## Tentang Kami
 
 Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
 
